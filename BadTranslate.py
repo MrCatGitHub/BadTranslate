39ec0296f48d5f3db4e9ff09f3436b5a4df93e6b
Mr. Cat <vonroman65@gmail.com>
2025-01-14 22:09:30 +0100

    y25w03b_pre6
    had to do the whole thing in async because google translate api is async

--- BadTranslate.py
+++ BadTranslate.py
@@ -2,42 +2,47 @@ import random
 import time
 from googletrans import Translator, LANGUAGES
 from tkinter import Tk
+import asyncio
 
 def random_language_code():
     languages = list(LANGUAGES.keys())
     return random.choice(languages)
 
-def translate_text(text, iterations=100):
+async def translate_text(text, iterations=100):
     translator = Translator()
+    translated = text
     for i in range(iterations):
         try:
             lang_code = random_language_code()
-            translated = translator.translate(text, dest=lang_code)
-            text = translator.translate(translated.text, dest='en').text
-            print(f"Iteration {i+1}/{iterations}: {text}")
+            random_trans = await translator.translate(translated, dest=lang_code)
+            eng_trans = await translator.translate(random_trans.text, dest='en')
+            translated = eng_trans.text
+            print(f"Iteration {i + 1}/{iterations}: {translated}")
         except Exception as e:
-            print(f"Error during translation at iteration {i+1}: {e}")
-            time.sleep(1)
-    return text
+            print(f"Error during translation at iteration {i + 1}: {e}")
+        time.sleep(1)
+    return translated
 
 def main():
     input_text = input("Enter the text you want to translate: ")
-    translated_text = translate_text(input_text)
-    print("Original text: ", input_text)
-    print("Translated text: ", translated_text)
-    Tk.withdraw()
-    Tk.clipboard_clear()
-    Tk.clipboard_append(translated_text)
-    Tk.update()
-    print('Copied result to clipboard.')
-    Tk.destroy()
+    translated_text = asyncio.run(translate_text(input_text, iterations=100))
+    print("Original text:", input_text)
+    print("Translated text:", translated_text)
+
+    #root = Tk()
+    #root.withdraw()
+    #root.clipboard_clear()
+    #root.clipboard_append(translated_text)
+    #root.update()
+    #root.destroy()
+    #print("Copied result to clipboard.")
 
 if __name__ == "__main__":
     main()
     yn = input('Done. Try another word? (y/n) ')
-    if yn == 'y' or 'Y':
+    if yn == 'y':
         main()
-    elif yn == 'n' or 'N':
+    elif yn == 'n':
         print ('Exiting now...')
         raise SystemExit()
     else:
